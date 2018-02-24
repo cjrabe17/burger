3f9ddc4b70abdb8807cad5f49755c94f43a21414
Cassie <cjrabe17@gmail.com>
2018-02-23 21:58:12 -0500

    started orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,14 +1,25 @@
-var connection = require("../config/connection.js");
+var connection = require("./connection.js");
 
 var orm = {
-    selectAll: function() {
-
+    // I dunno when this method gets used
+    selectAll: function(tableInput) {
+        var queryString = "SELECT * FROM burgers";
+        connection.query(queryString, function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
     },
-    insertOne: function() {
-
+    // For submitting a burger
+    insertOne: function(vals) {
+        var queryString = "INSERT INTO burgers (burger_name) VALUES (?)";
+        connection.query(queryString, vals, function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        })
     },
     updateOne: function() {
-        
+        // Is this for devouring?
+        // Change value to true?
     }
 }
 
