d205f8a59cd758802fd82946e1ebfc1552b39f48
Cassie <cjrabe17@gmail.com>
2018-02-25 22:23:43 -0500

    functionality finished

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,22 @@
 var orm = require("../config/orm.js");
+console.log("burger.js connected");
 
 var burger = {
-    // create the code that will call the ORM functions using burger specific input for the ORM
-}
+    all: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+    add: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    update: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    }
+};
 
 module.exports = burger;
